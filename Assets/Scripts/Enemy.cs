07152820c1eaad25978c46c14d992b1af2061dd1
Lucky7Tb <Luckytri101@gmail.com>
2022-03-14 14:04:23 +0700

    add blood effect, destroy enemy when is dead

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Animator animator;
+    public GameObject originalBlood;
+    GameObject cloneBlood;
     public float enemyHealth = 100f;
     public float enemyDamage = 5f;
     public float horizontalMove = 0f;
@@ -13,7 +15,9 @@ public class Enemy : MonoBehaviour
         enemyHealth -= damage;
 
         animator.SetTrigger("GetHit");
-
+        cloneBlood = Instantiate(originalBlood);
+        cloneBlood.transform.position = transform.position;
+        Destroy(cloneBlood, 0.5f);
         if(enemyHealth <= 0) {
             Die();
         }
@@ -25,6 +29,6 @@ public class Enemy : MonoBehaviour
         position.y = -2.8f;
         transform.position = position;
         GetComponent<Collider2D>().enabled = false;
-        this.enabled = false;
+        Destroy(this.gameObject, 2);
     }
 }
